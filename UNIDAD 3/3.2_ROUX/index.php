56c5a5ac2d4ecb2469c42e23c61ab76aa52ddebd
Cristal-Toscano <43659254+Cristal-Toscano@users.noreply.github.com>
2021-11-11 07:45:02 -0600

    Update index.php

--- UNIDAD 3/3.2_ROUX/index.php
+++ UNIDAD 3/3.2_ROUX/index.php
@@ -54,7 +54,7 @@ require('externosEncaPie/encabezado.php');
       <p>The first day of CAC events is kicked off under the theme of Art in Full Color. From a demonstration in graffiti art on a wall of the  Rousseau Room, to the exhibit of colorful glazed modern glassware in the Dover Hall.</p>
       <h3>Tuesday</h3>
       <p>Water in Art is the theme for the second day, as art students gather at the Fountain of Intrigue to create ice sculptures, and art lecturers discuss the use of water as an art material, and water as an art subject.</p>
-      <p><a class="link" href="schedule/index.php">Full Schedule</a></p>
+      <p><a class="link" href="schedule.php">Full Schedule</a></p>
     </article>
     <!-- Schedule --> 
   </aside>
