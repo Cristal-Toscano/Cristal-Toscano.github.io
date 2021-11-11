9ed7e5cde23cf9f48ef213e055be7be2fda2ae0e
Cristal-Toscano <43659254+Cristal-Toscano@users.noreply.github.com>
2021-11-10 21:34:55 -0600

    Add files via upload

---
+++ UNIDAD 3/3.2 ROUX/externosEncaPie/encabezado.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Roux Conference: Artists</title>
+<link rel="stylesheet" href="/Evidencia_3.2/css/style.css">
+</head>
+<body id="page_artists">
+<div class="wrapper">
+  <header>
+    <div class="branding"> <img src="/Evidencia_3.2/images/ralogo_monogram.png" alt="Logo">
+      <p>Roux Academy<br>
+        Art • Media • Design</p>
+    </div>
+    <div class="description">
+      <h1>Roux Academy 2016 Art Conference</h1>
+      <p>Join over 500 hundred of the most creative and brilliant minds of art colleges all around the world for five days of lectures by world-renowned art scholars and artists, and seven days and nights of gallery exhibits featuring the best in contemporary art, including painting, sculpture, and more, in the beautiful halls of Hotel Contempo in the heart of Seattle.</p>
+    </div>
+    <nav>
+      <ol>
+        <li><a href="/3.2_ROUX/index.php">HOME</a></li>
+        <li><a href="/3.2_ROUX/artists.php">ARTISTS</a></li>
+        <li><a href="/3.2_ROUX/schedule/index.php">SCHEDULE</a></li>
+        <li><a href="/3.2_ROUX/venue.php">VENUE/TRAVEL</a></li>
+        <li><a href="/3.2_ROUX/register/index.php">REGISTER</a></li>
+      </ol>
+    </nav>
+  </header>
+  <div id="rotator"> <img src="/3.2_ROUX/images/art01.jpg" alt="Artwork from show"> </div>
