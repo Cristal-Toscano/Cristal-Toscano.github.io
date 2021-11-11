a14b45a5cb89e0cfd7e6a51f5a080013d6cf2b30
Cristal-Toscano <43659254+Cristal-Toscano@users.noreply.github.com>
2021-11-11 00:21:37 -0600

    Update encabezado.php

--- UNIDAD 3/3.2_ROUX/externosEncaPie/encabezado.php
+++ UNIDAD 3/3.2_ROUX/externosEncaPie/encabezado.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <title>Roux Conference: Artists</title>
-<link rel="stylesheet" href="../css/style.css">
+<link rel="stylesheet" href="/css/style.css">
 
 </head>
 <body id="page_artists">
