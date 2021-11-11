0523a2de061c42cbdabb188fa7131ebd88d8f2af
Cristal-Toscano <43659254+Cristal-Toscano@users.noreply.github.com>
2021-11-11 00:05:51 -0600

    Update index.php

--- UNIDAD 3/3.2_ROUX/register/index.php
+++ UNIDAD 3/3.2_ROUX/register/index.php
@@ -1,5 +1,5 @@
 <?php
-require('../externosEncaPie/encabezado.php');
+require('../3.2_ROUX/externosEncaPie/encabezado.php');
 ?>
   <!-- Rotator -->
   <section id="main">
@@ -87,7 +87,7 @@ require('../externosEncaPie/encabezado.php');
   </aside>
   <!-- Sidebar -->
   <?php
-include('../externosEncaPie/piePagina.php');
+include('../3.2_ROUX/externosEncaPie/piePagina.php');
 ?>
 </div>
 </body>
